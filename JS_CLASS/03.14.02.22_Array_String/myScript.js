f2b246c6c86a95b16d9b3f6a70e156d3bebd0735
oleg <dundev1904@gmail.com>
2022-05-04 19:44:58 +0300

    Урок 3 Array_Strings /Массивы и строки/2

---
+++ JS_CLASS/03.14.02.22_Array_String/myScript.js
@@ -0,0 +1,8 @@
+console.log('Start Script')
+
+//alert('Кирилл ты не послушный мальчик')//Плохой стиль
+//let userName=prompt('Enter U Name', 'Vasya')
+//console.log('user enter: ' + userName)
+
+console.log('End Script')
+
